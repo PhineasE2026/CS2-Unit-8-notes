ab6f7f45f351fdae778dd499f6bc9000f97123ed
finny <phineas.ehrlich2026@gbwl.org>
2025-02-11 13:54:39 +0000

    twostrngmethod

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // declare 2d arrays with Type[][]
+      int[][] coordinates;
+      // CREATE 2D array of a certain number
+      // of rows and columns
+      coordinates = new int[5][5];
+      //initialize (declare + create)
+      String[][] seatingChart = new String[3][4];
+      //default values for String: null
+      // total num of elements = row*col = 12
+
+      //set values in a 2d array
+      seatingChart[0][0] = "Jackson";
+      seatingChart[1][0] = "Finny";
+      seatingChart[2][0] = "Bryce";
+      seatingChart[2][1] = "Natalie";
+      seatingChart[2][2] = "Paige";
+      seatingChart[2][3] = "Zoie";
+      seatingChart[1][3] = "Alex";
+      seatingChart[0][3] = "Maia";
    }
 }
